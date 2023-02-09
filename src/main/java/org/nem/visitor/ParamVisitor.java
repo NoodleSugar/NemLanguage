2b8e69731065b3e8c956b17c002294924f62f235
NoodleSugar <noodle@Noodle-PC.localdomain>
2023-02-09 19:39:48 +0100

    Ast construction tests

--- src/main/java/org/nem/visitor/ParamVisitor.java
+++ src/main/java/org/nem/visitor/ParamVisitor.java
@@ -2,6 +2,8 @@ package org.nem.visitor;
 
 import org.antlr.v4.runtime.tree.TerminalNode;
 import org.nem.ast.function.Parameter;
+import org.nem.ast.type.Type;
+import org.nem.ast.type.TypeFactory;
 import org.nem.grammar.NEMParser;
 import org.nem.grammar.NEMParserBaseVisitor;
 
@@ -9,9 +11,11 @@ public class ParamVisitor extends NEMParserBaseVisitor<Parameter> {
 
 	@Override
 	public Parameter visitParam(NEMParser.ParamContext ctx) {
-		TerminalNode id = ctx.IDENTIFIER();
-		TerminalNode colon = ctx.COLON();
-		NEMParser.Var_typeContext p = ctx.var_type();
-		return super.visitParam(ctx);
+		Type type = TypeFactory.get(ctx.var_type().getText());
+
+		if(ctx.IDENTIFIER() == null)
+			return new Parameter(type);
+
+		return new Parameter(type, ctx.IDENTIFIER().getText());
 	}
 }
--- src/test/java/org/nem/ast/AstComputerTest.java
+++ src/test/java/org/nem/ast/AstComputerTest.java
@@ -2,13 +2,13 @@ package org.nem.ast;
 
 import org.junit.Test;
 import org.nem.ast.function.Function;
+import org.nem.ast.function.Parameter;
 import org.nem.ast.type.TypeFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class AstComputerTest {
 	final String BASIC_FN_DEF = "basic_fn_def.nem";
@@ -21,15 +21,14 @@ public class AstComputerTest {
 		assertNotNull(content);
 		assertEquals(1, content.functions.size());
 
-		Function f = content.functions.get(0);
-
+		Function f = content.functions.get("f");
+		assertNotNull(f);
 		assertEquals("f", f.name);
 		assertEquals(TypeFactory.get("void"), f.returnType);
-
 		assertEquals(1, f.parameters.size());
 
 		Parameter p = f.parameters.get(0);
-		assertTrue(f.parameters.get(0).name.isPresent());
-		assertEquals(f.name);
+		assertTrue(p.name.isPresent());
+		assertEquals("p", p.name.get());
 	}
 }
